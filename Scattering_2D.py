b6d04ef83a32472326e5bdad001de770bcccaf19
elisha <elisha.langat@dlr.de>
2026-01-07 18:08:08 +0100

    implementation of scattering transform

--- Scattering_2D.py
+++ Scattering_2D.py
@@ -64,6 +64,54 @@ class ScatteringTransform:
         S2 = []
         
         #zeroth order
+        S0_coefficients = self._lowpass(x_fft)
+        S0.append(S0_coefficients)
+        
+        
+        #u1
+        U1_list = []
+        #compute first order coefficients
+        for j1 in range(self.J):
+            for l1 in range(self.L):
+                #convolve with wavelet psi[j1, l1]
+                U1 = self._wavelet_transform(x_fft, j1, l1)
+                
+                #apply modulus to the coefficients
+                U1_mod = torch.abs(U1)
+                U1_mod_fft = fft.fft2(U1_mod)
+                
+                #pass through low pass
+                S1_coeff = self._lowpass(U1_mod_fft)
+                
+                S1.append(S1_coeff)
+                
+                #store for second order
+                U1_list.append((U1_mod_fft, j1, l1))
+                
+        for u1_mod, j1, l1 in U1_list:
+            for j2 in range(j1 + 1, self.J):
+                for l2 in range(self.L):
+                    #convolve with wavelet
+                    U2 = self._wavelet_transform(U1_mod_fft, j2, l2)
+                    
+                    #apply modulus
+                    U2_mod = torch.abs(U2)
+                    U2_mod_fft = fft.fft2(U2_mod)
+                    
+                    #lowpass filter
+                    S2_coeff = self._lowpass(U2_mod_fft)
+                    S2.append(S2_coeff)
+                    
+        #stack coefficients
+        result = {
+            'S0': torch.stack(S0, dim=1),
+            'S1': torch.stack(S1, dim=1),
+            'S2': torch.stack(S2, dim=1)
+        }
+        
+        return result
+                
+                
         
         
     def _lowpass(self, x_fft):
@@ -85,4 +133,22 @@ class ScatteringTransform:
         return y
         
             
-            
+    def _wavelet_transform(self, x_fft, j, l):
+        '''
+        Docstring for _wavelet_transform
+        
+        :param self: Description
+        :param x_fft: innput signal
+        :param j: scale
+        :param l: orientation
+        '''
+        #select this specific filter
+        psi = self.filters['psi'][j, l].to(x_fft.device)
+        psi_complex = psi.to(self.cdtype)
+        
+        #multiply signal in fourier domain
+        y_fft = x_fft * psi_complex
+        #inverse fft
+        y = fft.ifft2(y_fft)
+        
+        return y
